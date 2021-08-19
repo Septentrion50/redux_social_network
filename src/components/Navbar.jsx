1ffd4cc869ed6d8231d132df4af7208686275811
Septentrion50 <erwan56lecorre@gmail.com>
2021-08-19 16:06:58 +0200

    Logout moved in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,18 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Logout from 'components/Logout'
 
 const Navbar = () => {
+
+  const user = useSelector((state) => state.auth) || {};
+
   return (
     <nav>
       <Link to="/">Home</Link>
       <Link to="/register">Register</Link>
       <Link to="/login">Login</Link>
       <Link to="/profile">Profile</Link>
+      {user.isAuthenticated ? <Logout/> : ''}
     </nav>
   )
 };
